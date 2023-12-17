a5ae2cf45b20671524e255796bf832d2ed1d7856
Jisung Lee <97920073+jskurt@users.noreply.github.com>
2023-12-17 10:49:34 +0900

    feat: list data and review (#1)

--- src/modules/github/components/BulkMergePrs.tsx
+++ src/modules/github/components/BulkMergePrs.tsx
@@ -1,27 +1,108 @@
 'use client'
 
-import type { FC } from 'react'
+import { type FC, useEffect, useState } from 'react'
+
+import { PullListByRepo } from '@/components/github/root/PullListByRepo'
+import { PullReviewForm } from '@/components/github/root/PullReviewForm'
 
 import { usePrsGroup } from '@/hooks/github/root/usePrsGroup'
 
+import type { TRepoHasCheck } from '@/types/github/root/index'
+
 export const BulkMergePrs: FC = () => {
   const { prsGroup } = usePrsGroup()
+  const [repoHasCheckArray, setRepoHasCheckArray] = useState<TRepoHasCheck[]>(
+    []
+  )
+
+  const handlePullChange = (repo: string, pullNumber: number) => {
+    setRepoHasCheckArray((prev) =>
+      prev.map((repoHasCheck) => {
+        if (repoHasCheck.repo === repo) {
+          return {
+            ...repoHasCheck,
+            pulls: repoHasCheck.pulls.map((pull) => {
+              if (pull.number === pullNumber) {
+                return { ...pull, isChecked: !pull.isChecked }
+              }
+
+              return pull
+            })
+          }
+        }
+        return repoHasCheck
+      })
+    )
+  }
+
+  const handleRepoChange = (repo: string) => {
+    setRepoHasCheckArray((prev) =>
+      prev.map((repoHasCheck) => {
+        if (repoHasCheck.repo === repo) {
+          if (
+            repoHasCheck.pulls.length > 0 &&
+            repoHasCheck.pulls.every((pull) => pull.isChecked === true)
+          ) {
+            return {
+              ...repoHasCheck,
+              pulls: repoHasCheck.pulls.map((pull) => ({
+                ...pull,
+                isChecked: false
+              }))
+            }
+          }
+
+          return {
+            ...repoHasCheck,
+            pulls: repoHasCheck.pulls.map((pull) => ({
+              ...pull,
+              isChecked: true
+            }))
+          }
+        }
+
+        return repoHasCheck
+      })
+    )
+  }
+
+  useEffect(() => {
+    if (!prsGroup) {
+      return
+    }
+
+    setRepoHasCheckArray(
+      prsGroup.map((pulls) => ({
+        org: pulls.org,
+        repo: pulls.repo,
+        pulls: pulls.pulls.map((pull) => ({
+          number: pull.number,
+          title: pull.title,
+          isChecked: false,
+          user: {
+            login: pull.user.login ?? ''
+          }
+        }))
+      }))
+    )
+  }, [prsGroup])
 
   return (
-    <div>
-      {(prsGroup ?? []).map((group) => (
-        <div key={`${group.org}-${group.repo}`}>
-          <h1 className="text-xl font-semibold">
-            {group.org} / {group.repo}
-          </h1>
-
-          <ol className="list-decimal ml-4 pl-10">
-            {group.pulls.map((pull) => (
-              <li key={pull.number}>{pull.title}</li>
-            ))}
-          </ol>
-        </div>
-      ))}
+    <div className="flex flex-wrap">
+      <div>
+        {repoHasCheckArray.map((repoHasCheck) => (
+          <PullListByRepo
+            key={repoHasCheck.repo}
+            repo={repoHasCheck.repo}
+            pulls={repoHasCheck.pulls}
+            handlePullChange={handlePullChange}
+            handleRepoChange={handleRepoChange}
+          />
+        ))}
+      </div>
+      <div className="ml-60">
+        <PullReviewForm repoHasCheckArray={repoHasCheckArray} />
+      </div>
     </div>
   )
 }
---
+++ src/modules/github/components/PullReviewForm.tsx
@@ -0,0 +1,125 @@
+import axios from 'axios'
+import { type ChangeEvent, type FC, useState } from 'react'
+
+import { EPullRequestType, type TRepoHasCheck } from '@/types/github/root/index'
+import type { PullReviewResponse } from '@/types/github/root/server'
+
+interface ICheckedPull {
+  org: string
+  repo: string
+  pullNumber: number
+}
+
+const getCheckedPullsInfo = (
+  repoHasCheckArray: TRepoHasCheck[]
+): ICheckedPull[] => {
+  return repoHasCheckArray.reduce<ICheckedPull[]>(
+    (reviewPullRequests, repoHasCheck) => {
+      const checkedPulls = repoHasCheck.pulls.filter((pull) => pull.isChecked)
+
+      if (checkedPulls.length === 0) {
+        return reviewPullRequests
+      }
+
+      return reviewPullRequests.concat(
+        checkedPulls.map((pull) => ({
+          org: repoHasCheck.org,
+          repo: repoHasCheck.repo,
+          pullNumber: pull.number
+        }))
+      )
+    },
+    []
+  )
+}
+
+interface PullReviewFormProps {
+  repoHasCheckArray: TRepoHasCheck[]
+}
+
+export const PullReviewForm: FC<PullReviewFormProps> = ({
+  repoHasCheckArray
+}) => {
+  const [commentInput, setCommentInput] = useState<string>('')
+  const hasChecked = repoHasCheckArray.some((data) =>
+    data.pulls.some((pull) => pull.isChecked === true)
+  )
+
+  const handleCommentChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCommentInput(event.target.value)
+  }
+
+  const reviewPullRequest = (
+    owner: string,
+    repo: string,
+    pullNumber: number,
+    event: EPullRequestType,
+    body: string
+  ) => {
+    axios
+      .post<PullReviewResponse>(
+        `/api/github/repos/${owner}/${repo}/pulls/${pullNumber}/reviews`,
+        {
+          event,
+          body
+        }
+      )
+      .then((response) => response.data)
+      .then((result) => {
+        console.log('result', result)
+      })
+      .catch(console.error)
+  }
+
+  const handleCommentClick = () => {
+    const checkedPullsInfo = getCheckedPullsInfo(repoHasCheckArray)
+
+    checkedPullsInfo.forEach((checkedPull) => {
+      reviewPullRequest(
+        checkedPull.org,
+        checkedPull.repo,
+        checkedPull.pullNumber,
+        EPullRequestType.COMMENT,
+        commentInput
+      )
+    })
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Leave a comment"
+        className="w-96 h-40"
+        value={commentInput}
+        onChange={handleCommentChange}
+        disabled={!hasChecked}
+      />
+      <div>
+        <button
+          className="bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          type="button"
+          disabled={!hasChecked}
+          onClick={handleCommentClick}
+        >
+          {EPullRequestType.COMMENT}
+        </button>
+
+        <button
+          className="mx-2 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          type="button"
+          disabled={!hasChecked}
+        >
+          {EPullRequestType.APPROVE}
+        </button>
+        <button
+          className="bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          type="button"
+          disabled={!hasChecked}
+        >
+          {EPullRequestType.REQUEST_CHANGES}
+        </button>
+      </div>
+    </div>
+  )
+}
