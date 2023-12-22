b812184f90945183513bcc878573080e488c0317
Jiyeol Lee <ka.felix496@gmail.com>
2023-12-22 16:10:28 -0500

    add collapsible

---
+++ src/elements/collapsible/collapsible.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import {
+  CollapsibleContent as RadixCollapsibleContent,
+  CollapsibleTrigger as RadixCollapsibleTrigger,
+  Root
+} from '@radix-ui/react-collapsible'
+
+const Collapsible = Root
+
+const CollapsibleTrigger = RadixCollapsibleTrigger
+
+const CollapsibleContent = RadixCollapsibleContent
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
