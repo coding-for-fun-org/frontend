9c35c1293c015cf8cf8f3fcaaaed3301743d3a1e
jskurt <jskurt9352@gmail.com>
2024-01-04 20:08:59 +0900

    changed from foreach to reduce

--- src/contexts/dictionary-provider.tsx
+++ src/contexts/dictionary-provider.tsx
@@ -20,28 +20,8 @@ export const DictionaryProvider: FC<DictionaryProviderProps> = ({
   children,
   dictionary
 }) => {
-  const translate = (key: string, params: ITranslateParams) => {
-    const keys = key.split('.')
-    let value = dictionary
-
-    for (const k of keys) {
-      value = value[k]
-      if (!value) {
-        return ''
-      }
-    }
-
-    if (params) {
-      Object.keys(params).forEach((paramKey) => {
-        const placeholder = `{${paramKey}}`
-        value = value.replace(new RegExp(placeholder, 'g'), params[paramKey])
-      })
-    }
-
-    return value
-  }
   return (
-    <DictionaryContext.Provider value={{ ...dictionary, translate }}>
+    <DictionaryContext.Provider value={dictionary}>
       {children}
     </DictionaryContext.Provider>
   )
@@ -49,9 +29,27 @@ export const DictionaryProvider: FC<DictionaryProviderProps> = ({
 
 export const useDictionary = () => {
   const dictionary = useContext(DictionaryContext)
+  const translate = (key: string, params: ITranslateParams) => {
+    const keys = key.split('.')
+    let result =
+      keys.reduce((acc, k) => {
+        if (!acc) {
+          return ''
+        }
+        return acc[k]
+      }, dictionary) || ''
+
+    if (params) {
+      result = Object.keys(params).reduce((acc, paramKey) => {
+        const placeholder = `{${paramKey}}`
+        return acc.replace(new RegExp(placeholder, 'g'), params[paramKey])
+      }, result)
+    }
+    return result
+  }
 
   return {
     dictionary,
-    translate: dictionary.translate
+    translate
   }
 }
