05e03eec1b20483d4cdf2d81ae691cfde0261afe
Jiyeol Lee <ka.felix496@gmail.com>
2024-01-11 21:55:47 -0500

    cancel fetching pulls if component is destroyed

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,3 +1,5 @@
+import type { AxiosRequestConfig } from 'axios'
+
 import { axiosGithub } from '@/utils/github/root/axios'
 
 import type { EPullRequestType } from '@/types/github/root/index'
@@ -9,26 +11,34 @@ import type {
 } from '@/types/github/root/server'
 
 export const githubService = {
-  async listUserInstallations(): Promise<UserInstallationsResponse> {
+  async listUserInstallations(
+    config?: AxiosRequestConfig
+  ): Promise<UserInstallationsResponse> {
     return axiosGithub
-      .get<UserInstallationsResponse>('/api/github/user/installations')
+      .get<UserInstallationsResponse>('/api/github/user/installations', config)
       .then((response) => response.data)
   },
   async listUserInstallationRepositories(
-    installationId: number
+    installationId: number,
+    config?: AxiosRequestConfig
   ): Promise<InstallationRepositoriesResponse> {
     return axiosGithub
       .get<InstallationRepositoriesResponse>(
-        `/api/github/user/installations/${installationId}/repositories`
+        `/api/github/user/installations/${installationId}/repositories`,
+        config
       )
       .then((response) => response.data)
   },
   async listPullRequests(
     owner: string,
-    repo: string
+    repo: string,
+    config?: AxiosRequestConfig
   ): Promise<RepoPullsResponse> {
     return axiosGithub
-      .get<RepoPullsResponse>(`/api/github/repos/${owner}/${repo}/pulls`)
+      .get<RepoPullsResponse>(
+        `/api/github/repos/${owner}/${repo}/pulls`,
+        config
+      )
       .then((response) => response.data)
   },
   async reviewPullRequest(
@@ -38,12 +48,14 @@ export const githubService = {
     payload: {
       reviewType: EPullRequestType
       comment: string
-    }
+    },
+    config?: AxiosRequestConfig
   ) {
     return axiosGithub
       .post<PullReviewResponse>(
         `/api/github/repos/${owner}/${repo}/pulls/${pullNumber}/reviews`,
-        { event: payload.reviewType, body: payload.comment }
+        { event: payload.reviewType, body: payload.comment },
+        config
       )
       .then((response) => response.data)
   }
