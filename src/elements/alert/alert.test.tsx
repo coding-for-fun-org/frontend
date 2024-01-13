1219de547ba6dc7bda070c218b9565891951acd0
Jiyeol Lee <41712713+kafelix496@users.noreply.github.com>
2024-01-13 16:42:46 -0500

    test: alert element (#74)

---
+++ src/elements/alert/alert.test.tsx
@@ -0,0 +1,69 @@
+import { Alert } from '@/elements/root/alert/alert'
+
+import { render, screen } from '@/utils/root/test/testing-library'
+
+describe('Alert', () => {
+  const ALERT_TITLE_TEXT = 'ALERT-TITLE-TEXT'
+  const ALERT_DESCRIPTION_TEXT = 'ALERT-DESCRIPTION-TEXT'
+
+  describe('variant primary or undefined', () => {
+    it('should render pin-icon (variant undefined)', () => {
+      render(<Alert title={ALERT_TITLE_TEXT} />)
+
+      expect(screen.getByTestId('pin-icon')).toBeInTheDocument()
+    })
+
+    it('should render pin-icon (variant primary)', () => {
+      render(<Alert variant="primary" title={ALERT_TITLE_TEXT} />)
+
+      expect(screen.getByTestId('pin-icon')).toBeInTheDocument()
+    })
+  })
+
+  describe('variant success', () => {
+    it('should render circle-icon', () => {
+      render(<Alert variant="success" title={ALERT_TITLE_TEXT} />)
+
+      expect(screen.getByTestId('circle-icon')).toBeInTheDocument()
+    })
+  })
+
+  describe('variant info', () => {
+    it('should render info-circled-icon', () => {
+      render(<Alert variant="info" title={ALERT_TITLE_TEXT} />)
+
+      expect(screen.getByTestId('info-circled-icon')).toBeInTheDocument()
+    })
+  })
+
+  describe('variant error', () => {
+    it('should render cross-circled-icon', () => {
+      render(<Alert variant="error" title={ALERT_TITLE_TEXT} />)
+
+      expect(screen.getByTestId('cross-circled-icon')).toBeInTheDocument()
+    })
+  })
+
+  it('should set className', () => {
+    const TEST_CLASS = 'TEST-CLASS'
+
+    render(<Alert className={TEST_CLASS} title={ALERT_TITLE_TEXT} />)
+
+    expect(screen.getByRole('alert')).toHaveClass(TEST_CLASS)
+  })
+
+  it('should not render description wrapper if description prop is undefined', () => {
+    render(<Alert title={ALERT_TITLE_TEXT} />)
+
+    expect(screen.queryByTestId('description')).not.toBeInTheDocument()
+  })
+
+  it('should render title and description as expected', () => {
+    render(
+      <Alert title={ALERT_TITLE_TEXT} description={ALERT_DESCRIPTION_TEXT} />
+    )
+
+    expect(screen.getByText(ALERT_TITLE_TEXT)).toBeInTheDocument()
+    expect(screen.getByText(ALERT_DESCRIPTION_TEXT)).toBeInTheDocument()
+  })
+})
