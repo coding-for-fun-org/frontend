24181f7384473e195b7b234e9f08b4798e491ac9
Jiyeol Lee <41712713+kafelix496@users.noreply.github.com>
2024-01-14 14:55:41 -0500

    test: add avatar element (#85)
    also update github workflow

---
+++ src/elements/avatar/avatar.test.tsx
@@ -0,0 +1,22 @@
+import { Avatar } from '@/elements/root/avatar/avatar'
+
+import { render, screen } from '@/utils/root/test/testing-library'
+
+describe('Avatar', () => {
+  it('should set img src and fallback', () => {
+    const FALLBACK_TEXT = 'AZ'
+    const IMAGE_SRC =
+      'https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+
+    render(<Avatar src={IMAGE_SRC} fallback={FALLBACK_TEXT} />)
+
+    const img = screen.getByRole('img', { name: FALLBACK_TEXT })
+
+    expect(img).toBeInTheDocument()
+
+    expect(img).toHaveAttribute(
+      'src',
+      expect.stringMatching(new RegExp(encodeURIComponent(IMAGE_SRC)))
+    )
+  })
+})
