9fec893ef9c508c47d08b4f806b282e7414672f0
Jisung Lee <97920073+jskurt@users.noreply.github.com>
2024-01-21 00:04:49 +0900

    feat(issue-39): show alert dialog when the user submit a review (#60)

--- src/modules/github/components/pull-review-form/pull-review-form.tsx
+++ src/modules/github/components/pull-review-form/pull-review-form.tsx
@@ -1,14 +1,18 @@
 import { type ChangeEvent, type FC, useState } from 'react'
 
+import { AlertDialog } from '@/elements/root/alert-dialog/alert-dialog'
+import { Alert } from '@/elements/root/alert/alert'
 import { Button } from '@/elements/root/button/button'
+import { Progress } from '@/elements/root/progress/progress'
 import { Textarea } from '@/elements/root/textarea/textarea'
 import { useToast } from '@/elements/root/toast/toast-provider'
 
 import { useDictionary } from '@/contexts/root/dictionary-provider/dictionary-provider'
 
-import { githubService } from '@/services/root/github'
-
-import { getCheckedPullsInfo } from '@/components/github/root/pull-review-form/utils'
+import {
+  getCheckedPullsInfo,
+  processSubmit
+} from '@/components/github/root/pull-review-form/utils'
 
 import { EPullRequestType, type TRepoHasCheck } from '@/types/github/root/index'
 
@@ -16,12 +20,40 @@ interface PullReviewFormProps {
   repoHasCheckArray: TRepoHasCheck[]
 }
 
+interface Errors {
+  repo: string
+  pullTitle: string
+}
+
 export const PullReviewForm: FC<PullReviewFormProps> = ({
   repoHasCheckArray
 }) => {
+  const [errors, setErrors] = useState<Errors[]>([])
   const [commentInput, setCommentInput] = useState<string>('')
-  const { pushToast } = useToast()
+  const [dialogData, setDialogData] = useState<
+    | {
+        open: true
+        type: EPullRequestType
+        title: string
+      }
+    | {
+        open: false
+        type?: never
+        title?: never
+      }
+  >({ open: false })
+  const [progressData, setProgressData] = useState<
+    | {
+        isRunning: true
+        value: number
+      }
+    | {
+        isRunning: false
+        value?: never
+      }
+  >({ isRunning: false })
   const { translate } = useDictionary()
+  const { pushToast } = useToast()
   const hasComment = commentInput.length > 0
   const hasChecked = repoHasCheckArray.some((data) =>
     data.pulls.some((pull) => pull.isChecked === true)
@@ -31,137 +63,158 @@ export const PullReviewForm: FC<PullReviewFormProps> = ({
     setCommentInput(event.target.value)
   }
 
-  const reviewPullRequest = (
-    owner: string,
-    repo: string,
-    pullTitle: string,
-    pullNumber: number,
-    reviewType: EPullRequestType,
-    comment: string
-  ) => {
-    githubService
-      .reviewPullRequest(owner, repo, pullNumber, { reviewType, comment })
-      .then(() => {
-        const translatedRepo = translate(
-          'GITHUB.PULL_REVIEW_FORM_SUBMIT_DESCRIPTION_REPO',
-          { repoName: repo }
-        )
-        const translatedPullTitle = translate(
-          'GITHUB.PULL_REVIEW_FORM_SUBMIT_DESCRIPTION_PULL',
-          { pullTitle: pullTitle }
-        )
+  const submit = (pullRequestType: EPullRequestType) => {
+    const checkedPullsInfo = getCheckedPullsInfo(repoHasCheckArray)
+    const progressIncreaseValue = 100 / checkedPullsInfo.length
 
-        pushToast({
-          title: translate('GITHUB.TOAST_SUCCESS_TITLE'),
-          description: (
-            <div>
-              <div>{translatedRepo}</div>
-              <div>{translatedPullTitle}</div>
-            </div>
-          ),
-          variant: 'success'
-        })
+    const handleProgressing = () => {
+      setProgressData((prev) => {
+        const value = prev.value! + progressIncreaseValue
+
+        return {
+          isRunning: true,
+          value: value > 100 ? 100 : value
+        }
       })
-      .catch(() => {
-        const translatedRepo = translate(
-          'GITHUB.PULL_REVIEW_FORM_SUBMIT_DESCRIPTION_REPO',
-          { repoName: repo }
-        )
-        const translatedPullTitle = translate(
-          'GITHUB.PULL_REVIEW_FORM_SUBMIT_DESCRIPTION_PULL',
-          { pullTitle: pullTitle }
-        )
+    }
 
-        pushToast({
-          title: translate('GITHUB.TOAST_ERROR_TITLE'),
-          description: (
-            <div>
-              <div>{translatedRepo}</div>
-              <div>{translatedPullTitle}</div>
-            </div>
-          ),
-          variant: 'error'
-        })
+    setProgressData({ isRunning: true, value: 0 })
+    processSubmit(
+      checkedPullsInfo,
+      handleProgressing,
+      pullRequestType,
+      commentInput
+    )
+      .then((result) => {
+        if (result.every((item) => item.status === 'fulfilled')) {
+          setDialogData({ open: false })
+          setProgressData({ isRunning: false })
+          pushToast({
+            title: translate('GITHUB.TOAST_SUCCESS_TITLE'),
+            variant: 'success'
+          })
+        } else {
+          const errors = result.reduce<Errors[]>((accu, item, index) => {
+            if (item.status === 'rejected') {
+              return accu.concat([
+                {
+                  repo: checkedPullsInfo[index]!.repo,
+                  pullTitle: checkedPullsInfo[index]!.pullTitle
+                }
+              ])
+            }
+
+            return accu
+          }, [])
+          setErrors(errors)
+        }
       })
+      .catch(console.error)
   }
 
-  const handleCommentClick = () => {
-    const checkedPullsInfo = getCheckedPullsInfo(repoHasCheckArray)
-
-    checkedPullsInfo.forEach((checkedPull) => {
-      reviewPullRequest(
-        checkedPull.org,
-        checkedPull.repo,
-        checkedPull.pullTitle,
-        checkedPull.pullNumber,
-        EPullRequestType.COMMENT,
-        commentInput
-      )
-    })
+  const handleOpenDialog = (type: EPullRequestType) => {
+    if (type === EPullRequestType.COMMENT) {
+      setDialogData({
+        open: true,
+        type,
+        title: translate('GITHUB.PULL_REVIEW_FORM_COMMENT_BUTTON')
+      })
+    } else if (type === EPullRequestType.APPROVE) {
+      setDialogData({
+        open: true,
+        type,
+        title: translate('GITHUB.PULL_REVIEW_FORM_APPROVE_BUTTON')
+      })
+    } else if (type === EPullRequestType.REQUEST_CHANGES) {
+      setDialogData({
+        open: true,
+        type,
+        title: translate('GITHUB.PULL_REVIEW_FORM_REQUEST_CHANGES_BUTTON')
+      })
+    }
   }
 
-  const handleApproveClick = () => {
-    const checkedPullsInfo = getCheckedPullsInfo(repoHasCheckArray)
-
-    checkedPullsInfo.forEach((checkedPull) => {
-      reviewPullRequest(
-        checkedPull.org,
-        checkedPull.repo,
-        checkedPull.pullTitle,
-        checkedPull.pullNumber,
-        EPullRequestType.APPROVE,
-        commentInput
-      )
-    })
+  const handleOpenChange = (open: boolean) => {
+    if (open === false) {
+      setDialogData({ open })
+      setProgressData({ isRunning: false })
+      setErrors([])
+    }
   }
 
-  const handleRequestChangeClick = () => {
-    const checkedPullsInfo = getCheckedPullsInfo(repoHasCheckArray)
-
-    checkedPullsInfo.forEach((checkedPull) => {
-      reviewPullRequest(
-        checkedPull.org,
-        checkedPull.repo,
-        checkedPull.pullTitle,
-        checkedPull.pullNumber,
-        EPullRequestType.REQUEST_CHANGES,
-        commentInput
-      )
-    })
+  const handleActionClick = () => {
+    if (!dialogData.open) {
+      return
+    }
+    submit(dialogData.type)
   }
 
   return (
-    <div className="flex flex-col h-full">
-      <Textarea
-        className="resize-none flex-grow"
-        placeholder={translate('GITHUB.PULL_REVIEW_FORM_COMMENT_PLACEHOLDER')}
-        value={commentInput}
-        onChange={handleCommentChange}
-        disabled={!hasChecked}
-      />
-
-      <div className="mt-5 flex justify-end gap-2">
-        <Button
-          type="button"
-          disabled={!hasChecked || !hasComment}
-          label={translate('GITHUB.PULL_REVIEW_FORM_COMMENT_BUTTON')}
-          onClick={handleCommentClick}
-        ></Button>
-
-        <Button
-          type="button"
+    <>
+      <div className="flex flex-col h-full">
+        <Textarea
+          className="resize-none flex-grow"
+          placeholder={translate('GITHUB.PULL_REVIEW_FORM_COMMENT_PLACEHOLDER')}
+          value={commentInput}
+          onChange={handleCommentChange}
           disabled={!hasChecked}
-          label={translate('GITHUB.PULL_REVIEW_FORM_APPROVE_BUTTON')}
-          onClick={handleApproveClick}
-        ></Button>
+        />
 
-        <Button
-          type="button"
-          disabled={!hasChecked || !hasComment}
-          label={translate('GITHUB.PULL_REVIEW_FORM_REQUEST_CHANGES_BUTTON')}
-          onClick={handleRequestChangeClick}
-        ></Button>
+        <div className="mt-5 flex justify-end gap-2">
+          <AlertDialog
+            open={dialogData.open}
+            onOpenChange={handleOpenChange}
+            onActionClick={handleActionClick}
+            title={dialogData.title}
+            children={
+              <>
+                {progressData.isRunning && (
+                  <Progress value={progressData.value} max={100} />
+                )}
+
+                {errors.length > 0 && (
+                  <Alert
+                    variant="error"
+                    title="error"
+                    description={
+                      <div>
+                        {errors.map((error, index) => (
+                          <div key={index}>
+                            <div>- repo: {error.repo}</div>
+                            <div>- pullTitle: {error.pullTitle}</div>
+                          </div>
+                        ))}
+                      </div>
+                    }
+                  />
+                )}
+              </>
+            }
+            actionLabel="Submit"
+          ></AlertDialog>
+
+          <Button
+            type="button"
+            disabled={!hasChecked || !hasComment}
+            label={translate('GITHUB.PULL_REVIEW_FORM_COMMENT_BUTTON')}
+            onClick={() => handleOpenDialog(EPullRequestType.COMMENT)}
+          ></Button>
+
+          <Button
+            type="button"
+            disabled={!hasChecked}
+            label={translate('GITHUB.PULL_REVIEW_FORM_APPROVE_BUTTON')}
+            onClick={() => handleOpenDialog(EPullRequestType.APPROVE)}
+          ></Button>
+
+          <Button
+            type="button"
+            disabled={!hasChecked || !hasComment}
+            label={translate('GITHUB.PULL_REVIEW_FORM_REQUEST_CHANGES_BUTTON')}
+            onClick={() => handleOpenDialog(EPullRequestType.REQUEST_CHANGES)}
+          ></Button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
