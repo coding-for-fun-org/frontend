9dc9072d38125bf35f287923779acb17e31280b5
Jisung Lee <jskurt9352@gmail.com>
2024-02-18 15:13:09 +0900

    make a layout in github

---
+++ src/app/(views)/github/layout.tsx
@@ -0,0 +1,9 @@
+import { type ReactNode } from 'react'
+
+import { IndexTabs } from '@/components/github/root/index-tabs/index-tabs'
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  console.log(children)
+
+  return <IndexTabs />
+}
