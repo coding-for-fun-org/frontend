961da42e4f6aa521a9a6dca587f8a394a5995072
Jisung Lee <jskurt9352@gmail.com>
2024-03-02 15:38:44 +0900

    remove dialog in pull-list-item.tsx

--- src/modules/github/components/pull-list-item/pull-list-item.tsx
+++ src/modules/github/components/pull-list-item/pull-list-item.tsx
@@ -1,20 +1,8 @@
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  ExternalLinkIcon
-} from '@radix-ui/react-icons'
 import Link from 'next/link'
-import { useState } from 'react'
 
-import { Button } from '@/elements/root/button/button'
 import { Checkbox } from '@/elements/root/checkbox/checkbox'
-import { Dialog } from '@/elements/root/dialog/dialog'
-import { Tooltip } from '@/elements/root/tooltip/tooltip'
-
-import { useDictionary } from '@/contexts/root/dictionary-provider/dictionary-provider'
 
 import { PullListItemStatus } from '@/components/github/root/pull-list-item-status/pull-list-item-status'
-import { PullReviewDialogBody } from '@/components/github/root/pull-review-dialog-body/pull-review-dialog-body'
 
 import type { TPull } from '@/types/github/root/index'
 
@@ -33,17 +21,6 @@ export const PullListItem = ({
   pull,
   handlePullCheckChange
 }: IPullListItemProps) => {
-  const { translate } = useDictionary()
-  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false)
-
-  const handleOpenChange = (open: boolean) => {
-    setIsDialogOpen(open)
-  }
-
-  const handleCancelClick = () => {
-    setIsDialogOpen(false)
-  }
-
   return (
     <div className="flex gap-2 items-center">
       <Checkbox
@@ -61,45 +38,6 @@ export const PullListItem = ({
       >
         <span>{pull.title}</span>
       </Link>
-
-      <Dialog
-        open={isDialogOpen}
-        onOpenChange={handleOpenChange}
-        title={pull.title}
-        children={
-          <div>
-            <div>
-              <ChevronRightIcon
-                onClick={() => console.log('next pull')}
-                className="float-right hover:underline cursor-pointer m-2"
-              />
-              <ChevronLeftIcon
-                onClick={() => console.log('previous pull')}
-                className="float-right hover:underline cursor-pointer m-2"
-              />
-              <Tooltip tooltip={translate('HEADER.LINK_GITHUB_TOOLTIP')}>
-                <Link href={pull.url} target="_blank">
-                  <ExternalLinkIcon
-                    onClick={() => console.log('link to pull request')}
-                    className="float-right hover:underline cursor-pointer m-2"
-                  />
-                </Link>
-              </Tooltip>
-            </div>
-            <PullReviewDialogBody description={pull.body} />
-          </div>
-        }
-        footer={
-          <>
-            <Button variant="outline" onClick={handleCancelClick}>
-              {translate('COMMON.ALERT_DIALOG_DEFAULT_CANCEL_BUTTON')}
-            </Button>
-            <Button variant="primary">
-              {translate('COMMON.DIALOG_REVIEW_BUTTON')}
-            </Button>
-          </>
-        }
-      />
 
       <div className="w-6 h-6 flex items-center flex-none">
         <PullListItemStatus
