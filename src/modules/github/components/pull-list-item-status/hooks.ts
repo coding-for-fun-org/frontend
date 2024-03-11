c1616020d8abfd410fbf720daa60bd0e32258cc4
Jisung Lee <jskurt9352@gmail.com>
2024-03-11 18:55:15 +0900

    test

--- src/modules/github/components/pull-list-item-status/hooks.ts
+++ src/modules/github/components/pull-list-item-status/hooks.ts
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
 
 import { useDictionary } from '@/contexts/root/dictionary-provider/dictionary-provider'
 
@@ -90,10 +91,25 @@ export const useCheckStatus = (
         })
       : null
 
-  if (checkStatus === ECheckStatus.RUNNING) {
-    console.log('check it again every 30 seconds')
+  const checkStatusAgain = () => {
+    console.log('checkStatus', checkStatus)
+    console.log('start checkStatusAgain')
+    getCheckStatus(requiredChecksName, checkRuns)
+    if (checkStatus === ECheckStatus.SUCCESS) {
+      console.log('finished')
+      return
+    }
+    setTimeout(() => {
+      console.log('run!')
+      checkStatusAgain()
+    }, 3000)
   }
 
+  useEffect(() => {
+    if (checkStatus === ECheckStatus.RUNNING) {
+      checkStatusAgain()
+    }
+  }, [checkStatus])
   return {
     checkStatus,
     checkStatusText,
