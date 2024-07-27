c93da444716bb356828a98a099bee96d6728e4a9
Jiyeol Lee <ka.felix496@gmail.com>
2024-07-26 20:56:54 -0400

    fix: add util functions
    isObjectLike, isPlainObject and isString

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,4 +1,9 @@
-import { createIdGenerator } from '@/utils/root/index'
+import {
+  createIdGenerator,
+  isObjectLike,
+  isPlainObject,
+  isString
+} from '@/utils/root/index'
 
 describe('index', () => {
   describe('createIdGenerator', () => {
@@ -18,6 +23,54 @@ describe('index', () => {
       expect(idGenerator()).toBe(7)
       expect(idGenerator()).toBe(8)
       expect(idGenerator()).toBe(9)
+    })
+  })
+
+  describe('isString', () => {
+    it('should return true when value is string', () => {
+      expect(isString('')).toBe(true)
+      expect(isString('abc')).toBe(true)
+    })
+
+    it('should return false when value is not string', () => {
+      expect(isString(null)).toBe(false)
+      expect(isString(undefined)).toBe(false)
+      expect(isString(0)).toBe(false)
+      expect(isString({})).toBe(false)
+      expect(isString([])).toBe(false)
+      expect(isString(true)).toBe(false)
+    })
+  })
+
+  describe('isObjectLike', () => {
+    it('should return true when value is object like', () => {
+      expect(isObjectLike({})).toBe(true)
+      expect(isObjectLike([])).toBe(true)
+    })
+
+    it('should return false when value is not object like', () => {
+      expect(isObjectLike(null)).toBe(false)
+      expect(isObjectLike(undefined)).toBe(false)
+      expect(isObjectLike(0)).toBe(false)
+      expect(isObjectLike('')).toBe(false)
+      expect(isObjectLike(true)).toBe(false)
+    })
+  })
+
+  describe('isPlainObject', () => {
+    it('should return true when value is plain object', () => {
+      expect(isPlainObject({})).toBe(true)
+      expect(isPlainObject({ a: 1 })).toBe(true)
+      expect(isPlainObject({ a: { b: 2 } })).toBe(true)
+    })
+
+    it('should return false when value is not plain object', () => {
+      expect(isPlainObject([])).toBe(false)
+      expect(isPlainObject(null)).toBe(false)
+      expect(isPlainObject(undefined)).toBe(false)
+      expect(isPlainObject(0)).toBe(false)
+      expect(isPlainObject('')).toBe(false)
+      expect(isPlainObject(true)).toBe(false)
     })
   })
 })
