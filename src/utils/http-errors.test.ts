1b726fb47c6e90f145ad364f0092817239f3ce1c
Jisung Lee <jskurt9352@gmail.com>
2024-08-08 20:24:01 +0900

    write a test code about handleHttpErrorResponse

--- src/utils/http-errors.test.ts
+++ src/utils/http-errors.test.ts
@@ -1,4 +1,7 @@
-import { createHttpError } from '@/utils/root/http-errors'
+import {
+  createHttpError,
+  handleHttpErrorResponse
+} from '@/utils/root/http-errors'
 
 describe('http-errors', () => {
   describe('createHttpError', () => {
@@ -32,6 +35,41 @@ describe('http-errors', () => {
       expect(error).toBeInstanceOf(Error)
       expect(error.status).toBe(404)
       expect(error.message).toBe(originalError.message)
+    })
+  })
+
+  describe('handleHttpErrorResponse', () => {
+    it(`should return an error with the correct status code, but we can't check response`, () => {
+      const error = {
+        status: 404,
+        response: {
+          data: {
+            message: 'Not Found',
+            documentation_url: 'https://test_documentation_url/coding-for-fun'
+          }
+        }
+      }
+
+      const parsedError = createHttpError(error.status, error)
+
+      const result = handleHttpErrorResponse(parsedError)
+      expect(result.status).toEqual(error.status)
+    })
+
+    it(`should return an error with the status code 400`, () => {
+      const error = {
+        response: {
+          data: {
+            message: 'Not Found',
+            documentation_url: 'https://test_documentation_url/coding-for-fun'
+          }
+        }
+      }
+
+      const parsedError = createHttpError(undefined, error)
+
+      const result = handleHttpErrorResponse(parsedError)
+      expect(result.status).toEqual(400)
     })
   })
 })
