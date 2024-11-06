a9661b7614056e7d3ca72fce9f29d5fe5214edb1
Jisung Lee <jskurt9352@gmail.com>
2024-11-06 10:13:05 +0900

    separate buttons and run with mock data

--- src/app/(views)/github/t/merge/page.tsx
+++ src/app/(views)/github/t/merge/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 
+import { Button } from '@/elements/root/button/button'
 import Stepper from '@/elements/root/stepper/stepper'
 
 const mockSteps = [
@@ -11,18 +12,28 @@ const mockSteps = [
 
 // just using a client component not to think too much about it
 export default function Page() {
-  const [currentStep, setCurrentStep] = useState(0)
-  const handleStepClick = (value: number) => {
-    setCurrentStep((prev) => prev + value)
+  const [currentStep, setCurrentStep] = useState<number>(0)
+  const handleStepPrev = () => {
+    setCurrentStep((prev) => prev - 1)
   }
+
+  const handleStepNext = () => {
+    setCurrentStep((prev) => prev + 1)
+  }
+
   return (
     <div>
       <h1>Merge page</h1>
-      <Stepper
-        currentStep={currentStep}
-        steps={mockSteps}
-        handleStepClick={handleStepClick}
-      />
+      <Stepper currentStep={currentStep} steps={mockSteps} />
+      <Button onClick={handleStepPrev} disabled={currentStep === 0}>
+        Prev
+      </Button>
+      <Button
+        onClick={handleStepNext}
+        disabled={currentStep === mockSteps.length - 1}
+      >
+        Next
+      </Button>
     </div>
   )
 }
--- src/elements/stepper/stepper.tsx
+++ src/elements/stepper/stepper.tsx
@@ -1,5 +1,3 @@
-import { Button } from '@/elements/root/button/button'
-
 interface Stepper {
   value: number
   label: string
@@ -8,27 +6,15 @@ interface Stepper {
 interface IStepperProps {
   currentStep: number
   steps: Stepper[]
-  handleStepClick: (value: number) => void
 }
 
-const Stepper = ({ currentStep, steps, handleStepClick }: IStepperProps) => {
-  const handleToPrevious = () => {
-    if (currentStep > 0) {
-      handleStepClick(-1)
-    }
-  }
-
+const Stepper = ({ currentStep, steps }: IStepperProps) => {
   return (
     <div>
       <>
         <div>
           <h3>Step {steps[currentStep]!.value}</h3>
           <div>{steps[currentStep]!.label}</div>
-        </div>
-        <div>
-          <Button onClick={handleToPrevious} disabled={currentStep === 0}>
-            Previous
-          </Button>
         </div>
       </>
     </div>
