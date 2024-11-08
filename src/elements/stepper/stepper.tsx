4827118d09d3981c3052e8b9de4dd4b40ec272d0
Jisung Lee <jskurt9352@gmail.com>
2024-11-08 22:56:36 +0900

    remove title

--- src/elements/stepper/stepper.tsx
+++ src/elements/stepper/stepper.tsx
@@ -1,21 +1,17 @@
-import { type ReactNode } from 'react'
-
 interface Stepper {
   value: number
   label: string
 }
 
 interface IStepperProps {
-  title: ReactNode
   currentStep: number
   steps: Stepper[]
 }
 
-export const Stepper = ({ title, currentStep, steps }: IStepperProps) => {
+export const Stepper = ({ currentStep, steps }: IStepperProps) => {
   return (
     <div>
       <div>
-        {title}
         <div>{steps[currentStep]!.label}</div>
       </div>
     </div>
