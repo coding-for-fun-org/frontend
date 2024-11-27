630b2acd208598072efae320124548d5cf0a892b
Jisung Lee <jskurt9352@gmail.com>
2024-11-27 10:38:33 +0900

    add checkbox in table

--- src/modules/github/components/pulls-sort-table/pulls-sort-table.tsx
+++ src/modules/github/components/pulls-sort-table/pulls-sort-table.tsx
@@ -1,5 +1,6 @@
 import type { TRepo } from 'src/modules/github/types'
 
+import { Checkbox } from '@/elements/root/checkbox/checkbox'
 import { Table } from '@/elements/root/table/table'
 
 interface IPullsSortTable {
@@ -7,6 +8,10 @@ interface IPullsSortTable {
 }
 
 export const PullsSortTable = ({ repos }: IPullsSortTable) => {
+  const handleCheckedChange = () => {
+    console.log(handleCheckedChange)
+  }
+
   if (!repos) {
     return null
   }
@@ -14,6 +19,12 @@ export const PullsSortTable = ({ repos }: IPullsSortTable) => {
     repo.pulls!.map((pull) => ({
       key: `row-${repo.name}-${pull.number}`,
       items: [
+        {
+          key: `cell-checkbox-${pull.number}`,
+          children: (
+            <Checkbox checked={false} onCheckedChange={handleCheckedChange} />
+          )
+        },
         {
           key: `cell-${repo.name}`,
           children: repo.name
@@ -34,11 +45,15 @@ export const PullsSortTable = ({ repos }: IPullsSortTable) => {
             key: 'header',
             items: [
               {
-                key: 'header-cell-0',
-                children: 'Repository'
+                className: 'w-3 content-start',
+                key: 'header-cell-0'
               },
               {
                 key: 'header-cell-1',
+                children: 'Repository'
+              },
+              {
+                key: 'header-cell-2',
                 children: 'Pull request'
               }
             ]
